5627ffb2d227f78d39d81b747289936b1520ef3a
Bakonyi Gergő <bakonyi.gergo.istvan@gmail.com>
2020-10-17 23:07:12 +0200

    Initial java

---
+++ java/tiktokfollowercrawlers/src/main/java/hu/haku/tiktokfollowercrawlers/TiktokfollowercrawlersApplication.java
@@ -0,0 +1,13 @@
+package hu.haku.tiktokfollowercrawlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokfollowercrawlersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktokfollowercrawlersApplication.class, args);
+	}
+
+}
---
+++ java/tiktokfollowercrawlers/src/test/java/hu/haku/tiktokfollowercrawlers/TiktokfollowercrawlersApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.haku.tiktokfollowercrawlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokfollowercrawlersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
